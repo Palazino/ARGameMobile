cd2448642c3f265076c25ec96b743b92df1589d3
BeiNicolas <bei.nicolas8484@gmail.com>
2024-11-08 16:52:45 +0100

    SAVEHealthBarPLayer

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int maxHealth = 100;
+    public int currentHealth;
+    public Slider healthSlider;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        UpdateHealthSlider();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth < 0)
+        {
+            currentHealth = 0;
+        }
+        UpdateHealthSlider();
+    }
+
+    public void Heal(int amount)
+    {
+        currentHealth += amount;
+        if (currentHealth > maxHealth)
+        {
+            currentHealth = maxHealth;
+        }
+        UpdateHealthSlider();
+    }
+
+    void UpdateHealthSlider()
+    {
+        healthSlider.value = (float)currentHealth / maxHealth;
+    }
+}
