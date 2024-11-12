6a330ca51d8014f107b2fdfc950076582570a9d0
BeiNicolas <bei.nicolas8484@gmail.com>
2024-11-12 16:51:44 +0100

    SaveSTARTsoUND

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,10 +1,22 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public AudioSource audioSource; // Référence à l'AudioSource
+    public string sceneToLoad = "03_ARStage_1"; // Nom de la scène à charger
+    public float delay = 0.5f; // Délai avant de changer de scène
+
     public void StartGame()
     {
-        SceneManager.LoadScene("03_ARStage_1");
+        StartCoroutine(PlaySoundAndLoadScene());
+    }
+
+    private IEnumerator PlaySoundAndLoadScene()
+    {
+        audioSource.Play();
+        yield return new WaitForSeconds(audioSource.clip.length + delay); // Attendre la fin du son et le délai
+        SceneManager.LoadScene(sceneToLoad);
     }
 }
